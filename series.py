21f5800a819503a2d4238466eca624c9d78f37ea
nimaaskarian <nimaas1400@gmail.com>
2023-01-30 01:52:06 +0330

    no ocd when listing

--- series.py
+++ series.py
@@ -224,20 +224,22 @@ if "-c" in args:
     except IndexError as e:
         print("Invalid arguments!")
 if not len(args) or "-l" in args:
-    for item in listdir():
+    for i,item in enumerate(listdir()):
         f = open(getFile(item),"r")
         parsedData = parseData((f.read()))
         watchedAll = getWatchedAll(parsedData)
         print(item+":")
         print("{all} episodes, {percentage}% watched. next is {next}".format(item=item, all=watchedAll[1],percentage=getPercentage(*watchedAll),next=nextEpisode(parsedData)))
         # print(f"{item}, {watchedAll[0]} episodes, {getPercentage(*watchedAll)}% watched. next is S{lastSeason}E{parsedData[lastSeason-1][0]}")
-        print()
+        if i+1 != len(listdir()):
+            print()
 if "-L" in args:
-    for item in listdir():
+    for i,item in enumerate(listdir()):
         f = open(getFile(item),"r")
         parsedData = parseData((f.read()))
         printParsed(parsedData,item)
-        print()
+        if i+1 != len(listdir()):
+            print()
 if "-e" in args:
     index=args.index("-e")
     if index==len(args)-1:
