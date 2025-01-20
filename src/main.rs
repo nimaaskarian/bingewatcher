ed495846b53c98b9a2f1bc7f01419f55f672cb79
nimaaskarian <nimaaskarian.work@outlook.com>
2025-01-20 12:52:10 +0330

    Add utils.rs - Move utility functions to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-mod onlineserie;
-mod serie;
-
-use clap::{Command, CommandFactory, Parser};
-use clap_complete::{generate, Generator, Shell};
-use onlineserie::{online_tv_show, request_detail};
-use serie::{Serie, SeriePrint};
+// vim:foldmethod=marker
+// imports{{{
+use clap::{CommandFactory, Parser};
+use clap_complete::Shell;
 use std::{
-    cell::RefCell, fs, io::{self, Write}, path::{Path, PathBuf}, process
+    fs, io::{self, Write}, path::PathBuf, process
 };
 
-use home::home_dir;
-
-#[inline(always)]
-pub fn append_home_dir(strs: &[&str]) -> PathBuf {
-    let mut out = home_dir().unwrap();
-    for str in strs {
-        out.push(str);
-    }
-    out
-}
+mod utils;
+mod onlineserie;
+mod serie;
+use serie::{Serie, SeriePrint};
+use onlineserie::{online_tv_show, request_detail};
+//}}}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -26,61 +19,54 @@ struct Args {
     /// Search a query among the series
     #[arg(short, long)]
     search: Option<String>,
-
     /// Add watched
     #[arg(short = 'a', long, default_value_t = 0)]
     watch: usize,
-
     /// Remove watched
     #[arg(short = 'r', long, default_value_t = 0)]
     unwatch: usize,
-
     /// Extended series view, including each episode
     #[arg(short = 'E', long, default_value_t = false)]
     extended: bool,
-
     #[arg(short='c', long)]
     completion: Option<Shell>,
-
     /// Show next episode when printing
     #[arg(short = 'e', long, default_value_t = false)]
     episode: bool,
-
     /// Delete selected series
     #[arg(short, long, default_value_t = false)]
     delete: bool,
-
     /// Delete selected series without asking for confirmation
     #[arg(short = 'D', long, default_value_t = false)]
     delete_noask: bool,
-
     /// Print current season of selected series
     #[arg(short = 'p', long, default_value="normal")]
     print_mode: SeriePrint,
-
     /// Add an series from episodate API (needs internet)
     #[arg(short='o', long, default_value_t=String::new())]
     add_online: String,
-
     /// Show details of a series from episodate API (needs internet)
     #[arg(long, default_value_t=String::new())]
     detail_online: String,
-
     /// Search series from episodate API (needs internet)
     #[arg(short='O', long, default_value_t=String::new())]
     search_online: String,
-
     /// Show finished too
     #[arg(short, long, default_value_t = false)]
     finished: bool,
-
     /// Show finished too
     #[arg(short = 'F', long, default_value_t = false)]
     only_finished: bool,
-
     /// Selected indexes
     #[arg(last = true)]
     indexes: Vec<usize>,
+}
+
+enum AppMode {
+    PrintCompletions(Shell),
+    SearchOnline,
+    DetailOnline,
+    ListOrManipulate,
 }
 
 impl Args {
@@ -94,13 +80,13 @@ impl Args {
         if !self.detail_online.is_empty() {
             return AppMode::DetailOnline
         }
-        let dir = append_home_dir(&[".cache", "bingewatcher"]);
+        let dir = utils::append_home_dir(&[".cache", "bingewatcher"]);
         let mut series: Vec<Serie> = if self.only_finished {
-            read_dir_for_series(&dir,Some(Serie::is_finished))
+            utils::read_series_dir(&dir,Some(Serie::is_finished))
         } else if !self.finished {
-            read_dir_for_series(&dir,Some(Serie::is_not_finished))
+            utils::read_series_dir(&dir,Some(Serie::is_not_finished))
         } else {
-            read_dir_for_series(&dir,None)
+            utils::read_series_dir(&dir,None)
         };
         if !self.add_online.is_empty() {
             if let Ok(serie) = request_detail(&self.add_online).await {
@@ -121,13 +107,45 @@ impl Args {
             }
         }
         if !self.indexes.is_empty() {
-            return AppMode::ManipulateSeries(RefCell::new(series), RefCell::new(dir));
+            self.manipulate_series(&mut series, dir);
         } else {
             self.list_series(&series);
         }
-        AppMode::Nothing
+        AppMode::ListOrManipulate
     }
 
+    #[inline]
+    fn manipulate_series(&self, series: &mut Vec<Serie>, dir: PathBuf) {
+        for &index in &self.indexes {
+            let current_serie = &mut series[index];
+            if self.watch > self.unwatch {
+                current_serie.watch(self.watch-self.unwatch);
+                let watched_count = self.watch-self.unwatch;
+                println!("Watched {watched_count} episode(s) from {}.", current_serie.name);
+            } else {
+                let unwatch_count = self.unwatch-self.watch;
+                current_serie.unwatch(unwatch_count);
+                println!("Unwatched {} episode(s) from {}.",current_serie.name, unwatch_count);
+            }
+            current_serie.print(&self.print_mode);
+            current_serie.write_in_dir(&dir);
+            if self.delete || self.delete_noask {
+                let mut input = String::from("y");
+                if !self.delete_noask {
+                    print!("Do you want to delete \"{}\" [Y/n] ", current_serie.name);
+                    io::stdout().flush();
+                    io::stdin().read_line(&mut input);
+                }
+                if input.trim() != "n" {
+                    let path = &dir.join(&current_serie.filename());
+                    fs::remove_file(path);
+                    println!("Deleted {}", path.to_str().unwrap());
+                }
+            }
+        }
+    }
+
+    #[inline]
     fn list_series(&self, series: &[Serie]) {
         for (index, serie) in series.iter().enumerate() {
             print!("{index}: ");
@@ -136,50 +154,13 @@ impl Args {
     }
 }
 
-enum AppMode {
-    PrintCompletions(Shell),
-    SearchOnline,
-    DetailOnline,
-    ManipulateSeries(RefCell<Vec<Serie>>, RefCell<PathBuf>),
-    Nothing,
-}
-
-#[inline]
-fn read_dir_for_series(dir: &Path, filter: Option<fn(&Serie) -> bool>) -> Vec<Serie> {
-    std::fs::create_dir_all(dir);
-    if let Ok(dir) = fs::read_dir(dir) {
-        let iter = dir.flat_map(|entry| Serie::from_file(&entry.expect("File error").path()));
-        if let Some(filter) = filter {
-            iter.filter(filter).collect()
-        } else {
-            iter.collect()
-        }
-    } else {
-        vec![]
-    }
-}
-
-#[inline]
-fn print_watched_count(watch: usize, unwatch: usize, name: &str) {
-    let watched_count: isize = watch as isize - unwatch as isize;
-    match watched_count {
-        ..=-1 => println!("Unwatched {} episode(s) from {name}.", -watched_count),
-        1.. => println!("Watched {watched_count} episode(s) from {name}."),
-        _ => {}
-    }
-}
-
-fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
-    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
-}
-
-use AppMode::*;
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    use AppMode::*;
     let mut args = Args::parse();
     match args.app_mode().await {
         PrintCompletions(shell) => {
-            print_completions(shell, &mut Args::command());
+            utils::print_completions(shell, &mut Args::command());
         }
         SearchOnline => {
             let _ = online_tv_show(args.search_online).await;
@@ -190,31 +171,7 @@ async fn main() -> io::Result<()> {
                 process::exit(0);
             }
         }
-        ManipulateSeries(series, dir) => {
-            for index in args.indexes {
-                let current_serie = &mut series.borrow_mut()[index];
-                current_serie.watch(args.watch);
-                current_serie.unwatch(args.unwatch);
-                print_watched_count(args.watch, args.unwatch, &current_serie.name);
-
-                current_serie.print(&args.print_mode);
-                current_serie.write_in_dir(&dir.borrow())?;
-                if args.delete || args.delete_noask {
-                    let mut input = String::from("y");
-                    if !args.delete_noask {
-                        print!("Do you want to delete \"{}\" [Y/n] ", current_serie.name);
-                        io::stdout().flush();
-                        io::stdin().read_line(&mut input)?;
-                    }
-                    if input.trim() != "n" {
-                        let path = &dir.borrow().join(&current_serie.filename());
-                        fs::remove_file(path)?;
-                        println!("Deleted {}", path.to_str().unwrap());
-                    }
-                }
-            }
-        }
-        Nothing => {}
+        ListOrManipulate => {}
     }
     Ok(())
 }
---
+++ src/utils.rs
@@ -0,0 +1,35 @@
+use super::Serie;
+use home::home_dir;
+use clap::Command;
+use clap_complete::{generate, Generator};
+use std::{
+    fs, io::{self}, path::{Path, PathBuf},
+};
+
+#[inline(always)]
+pub fn append_home_dir(strs: &[&str]) -> PathBuf {
+    let mut out = home_dir().unwrap();
+    for str in strs {
+        out.push(str);
+    }
+    out
+}
+
+#[inline]
+pub fn read_series_dir(dir: &Path, filter: Option<fn(&Serie) -> bool>) -> Vec<Serie> {
+    std::fs::create_dir_all(dir);
+    if let Ok(dir) = fs::read_dir(dir) {
+        let iter = dir.flat_map(|entry| Serie::from_file(&entry.expect("File error").path()));
+        if let Some(filter) = filter {
+            iter.filter(filter).collect()
+        } else {
+            iter.collect()
+        }
+    } else {
+        vec![]
+    }
+}
+
+pub fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+}
