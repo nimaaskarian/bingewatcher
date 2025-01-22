b503ee1a0fc81305bf972168188d0284abf27f31
nimaaskarian <nimaaskarian.work@outlook.com>
2025-01-22 22:03:55 +0330

    Add benches - Read bench - Search bench

--- benches/bench_series.rs
+++ benches/bench_series.rs
@@ -18,5 +18,26 @@ fn read_system_series(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches,write, read_system_series);
+fn read_system_series_finished(c: &mut Criterion) {
+    let mut args = bw::args::Args::parse_from(["-F"]);
+    c.bench_function("read system series finished", |b| b.iter(||{
+        black_box(&mut args).app_mode()
+    }));
+}
+
+fn read_system_series_all(c: &mut Criterion) {
+    let mut args = bw::args::Args::parse_from(["-f"]);
+    c.bench_function("read system series all", |b| b.iter(||{
+        black_box(&mut args).app_mode()
+    }));
+}
+
+fn search_system_series(c: &mut Criterion) {
+    let mut args = bw::args::Args::parse_from(["-s invincible"]);
+    c.bench_function("read system series all", |b| b.iter(||{
+        black_box(&mut args).app_mode()
+    }));
+}
+
+criterion_group!(benches,write, read_system_series, read_system_series_finished, read_system_series_all, search_system_series);
 criterion_main!(benches);
